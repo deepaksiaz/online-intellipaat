27771887b417666bcc7f848c66f110eb72c60178
React Lab Files <95652257+reactfiles@users.noreply.github.com>
2022-11-12 20:38:13 +0530

    Create index.js

---
+++ day5-react-fun/steps/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { createRoot } from "react-dom/client"
+
+/* 
+let message = "Welcome to your life";
+let element = React.createElement("h1",null,message); 
+*/
+// class based components
+/* 
+class ClassComp extends Component{
+    message = "Welcome to your life | Class Component";
+    render(){
+        return <h1>{ this.message }</h1>
+    }
+} 
+*/
+// function based components 
+let FunComp = () => {
+    let message = "Welcome to your life | Function Component";
+    return <h1>{ message }</h1>
+}
+
+createRoot(document.getElementById("root")).render(<FunComp/>);
